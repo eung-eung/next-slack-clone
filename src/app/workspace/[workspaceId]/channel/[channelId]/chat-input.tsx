a76e58b1ed50f0b2357d9689b06ec1e75c2860e7
eung-eung <boyvip5231@gmail.com>
2024-09-11 01:35:37 +0700

    build chat-input component

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,11 +1,28 @@
-import Editor from "@/components/editor"
+import dynamic from "next/dynamic"
+import Quill from "quill"
+import { useRef } from "react"
 
+// Editor should not be in severside rendering => ssr:false
+//dynamic: will render later, not in serverside rendering
+const Editor = dynamic(() => import("@/components/editor"), { ssr: false })
 
+interface ChatInputProps {
+    placeholder: string,
 
-export const ChatInput = () => {
+}
+
+export const ChatInput = ({ placeholder }: ChatInputProps) => {
+    const editorRef = useRef<Quill | null>(null)
+
     return (
         <div className="px-5 w-full">
-            <Editor />
+            <Editor
+                variant="create"
+                placeholder={placeholder}
+                onSubmit={() => { }}
+                disabled={false}
+                innerRef={editorRef}
+            />
         </div>
     )
 }
