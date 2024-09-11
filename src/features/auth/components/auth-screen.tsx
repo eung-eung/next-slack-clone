7c11135cb16a6e65a6b584b2b7317e02c10dd5c3
eung-eung <boyvip5231@gmail.com>
2024-09-11 15:54:18 +0700

    add background beam

--- src/features/auth/components/auth-screen.tsx
+++ src/features/auth/components/auth-screen.tsx
@@ -4,12 +4,17 @@ import { useState } from "react"
 import { SignInFlow } from "../types"
 import { SignInCard } from "./sign-in-card"
 import { SignUpCard } from "./sign-up-card"
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision"
 
 export const AuthScreen = () => {
     const [state, setState] = useState<SignInFlow>('signIn')
-    return <div className="h-full flex items-center justify-center bg-[#5C3B58]">
-        <div className="md:h-auto md:w-[420px]">
-            {state === 'signIn' ? <SignInCard setState={setState} /> : <SignUpCard setState={setState} />}
-        </div>
-    </div>
+    return (
+        <BackgroundBeamsWithCollision
+            className="h-full flex items-center justify-center"
+        >
+            <div className="md:h-auto md:w-[420px] z-20">
+                {state === 'signIn' ? <SignInCard setState={setState} /> : <SignUpCard setState={setState} />}
+            </div>
+        </BackgroundBeamsWithCollision>
+    )
 }
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -50,7 +50,14 @@ export const SignUpCard = ({ setState }: SignUpCardProps) => {
     return <Card className="w-full h-full p-8">
         <CardHeader className="px-0 pt-0">
             <CardTitle>
-                Sign up to continue
+                <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+                    <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+                        <span className="text-lg">Sign up to continue</span>
+                    </div>
+                    <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+                        <span className="text-lg">Sign up to continue</span>
+                    </div>
+                </div>
             </CardTitle>
             <CardDescription>
                 Use your email or another service to continue
