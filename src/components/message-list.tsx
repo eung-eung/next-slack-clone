31ce1c52246c490a8e6104f36e86c1ea18127d6c
eung-eung <boyvip5231@gmail.com>
2025-01-03 15:22:54 +0700

    handle loading and infinity scroll messages

--- src/components/message-list.tsx
+++ src/components/message-list.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react'
 import { Id } from '../../convex/_generated/dataModel'
 import { useWorkspaceId } from '@/hooks/use-workspace-id'
 import { useCurrentMember } from '@/features/members/api/use-current-member'
+import { Loader } from 'lucide-react'
 
 const TIME_THRESHOLD = 5
 
@@ -106,6 +107,34 @@ export default function MessageList({
                     })}
                 </div>
             ))}
+            {/* observer infinity scroll */}
+            <div className='h-1'
+                ref={(el) => {
+                    console.log('ele: ', el);
+
+                    if (el) {
+                        const observer = new IntersectionObserver(([entry]) => {
+                            console.log("entry: ", entry);
+
+                            if (entry.isIntersecting && canLoadMore) {
+                                loadMore()
+                            }
+                        }, {
+                            threshold: 1.0
+                        })
+                        observer.observe(el)
+                        return () => observer.disconnect()
+                    }
+                }}
+            />
+            {isLoadingMore && (
+                <div className='text-center my-2 relative'>
+                    <hr className='absolute top-1/2 left-0 right-0 border-t border-gray-300'></hr>
+                    <span className='relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm'>
+                        <Loader className='size-4 animate-spin' />
+                    </span>
+                </div>
+            )}
             {variant === "channel" && channelName && channelCreationTime && (
                 <ChannelHero
                     name={channelName}
