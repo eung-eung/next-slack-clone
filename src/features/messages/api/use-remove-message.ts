ebf5141a03711f6a953a85d34f6e1c8bce14802e
eung-eung <boyvip5231@gmail.com>
2025-01-03 15:21:51 +0700

    remove message hook

---
+++ src/features/messages/api/use-remove-message.ts
@@ -0,0 +1,67 @@
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useCallback, useMemo, useState } from "react";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+type RequestType = {
+    id: Id<"messages">,
+}
+type ResponseType = Id<"messages"> | null
+
+type Options = {
+    onSuccess?: (data: ResponseType) => void,
+    onError?: (error: Error) => void,
+    onSettled?: () => void,
+    throwError?: boolean
+}
+
+export const useRemoveMessage = () => {
+    const [data, setData] = useState<ResponseType>(null)
+    const [error, setError] = useState<Error | null>(null)
+    const [status, setStatus] = useState<"error" | "success" | "pending" | "settled" | null>(null)
+
+    const isPending = useMemo(() => {
+        return status === "pending"
+
+    }, [status])
+    const isError = useMemo(() => {
+        return status === 'error'
+    }, [status])
+    const isSettled = useMemo(() => {
+        return status === 'settled'
+    }, [status])
+    const isSuccess = useMemo(() => {
+        return status === 'pending'
+    }, [status])
+
+    const mutation = useMutation(api.messages.remove)
+    const mutate = useCallback(async (values: RequestType, options?: Options) => {
+        try {
+            setData(null)
+            setError(null)
+            setStatus('pending')
+
+            const response = await mutation(values)
+            options?.onSuccess?.(response)
+            return response
+        } catch (error) {
+            setStatus('error')
+            options?.onError?.(error as Error)
+            if (options?.throwError) {
+                throw error
+            }
+        } finally {
+            setStatus('settled')
+            options?.onSettled?.()
+        }
+    }, [mutation])
+    return {
+        mutate,
+        error,
+        data,
+        isError,
+        isPending,
+        isSuccess,
+        isSettled
+    }
+}
